b7df455758474e6dfbba48fe86d142dabb9ef8c0
matcool <26722564+matcool@users.noreply.github.com>
2019-02-16 14:44:11 -0300

    basic timer,run and segment

---
+++ pysplit.py
@@ -0,0 +1,77 @@
+"""
+reminders:
+use snake case because yes
+always try to use ' over " 
+
+"""
+import time
+from enum import Enum
+# constants
+TIMING_FUNCTION = time.perf_counter
+
+class TimerState(Enum):
+    NOTHING = 0
+    RUNNING = 1
+    ENDED = 2
+    # not yet implemented
+    PAUSED = 3
+
+class Timer:
+    def __init__(self, run=None):
+        self.start_time = None
+        self.end_time = None
+        self.run = run
+        self.times = []
+        self.state = TimerState.NOTHING
+
+    def start(self, force=False):
+        if not force and self.state != TimerState.NOTHING:
+            raise Exception(f'Timer is currently {self.state.name.lower()}, please reset, finish the run or pass in force=True')
+        self.start_time = TIMING_FUNCTION()
+        self.end_time = None
+        self.times = []
+        self.last_time = 0
+        self.state = TimerState.RUNNING
+
+    def time(self):
+        if self.state == TimerState.NOTHING:
+            raise Exception('Timer is not currently running.')
+        return (self.end_time if self.state == TimerState.ENDED else TIMING_FUNCTION()) - self.start_time
+
+    def split(self):
+        if self.state != TimerState.RUNNING:
+            raise Exception('Please start the timer before splitting.')
+        t = self.time()
+        self.times.append(t - self.last_time)
+        self.last_time = t
+        if self.run == None or len(self.run.segments) == 0 or len(self.times) == len(self.run.segments):
+            self.state = TimerState.ENDED
+            self.end_time = t + self.start_time
+
+    def reset(self, save=True):
+        if save and self.run != None:
+            for i,t in enumerate(self.times):
+                self.run.segments[i].add_time(t)
+
+        self.state = TimerState.NOTHING
+        self.times = []
+        self.start_time = None
+        self.end_time = None
+
+
+class Run:
+    def __init__(self, name='', category=''):
+        self.name = name
+        self.category = category
+        self.segments = []
+
+class Segment:
+    def __init__(self, name=''):
+        self.name = name
+        self.history = []
+        self.best = None
+
+    def add_time(self, time):
+        if self.best == None or time < self.best:
+            self.best = time
+        self.history.append(time)
---
+++ test.py
@@ -0,0 +1,19 @@
+from pysplit import *
+
+run = Run(name='Something',category='Any%')
+
+to_type = ['Hello','World']
+for i in to_type:
+    run.segments.append(Segment(name='Type '+i))
+
+timer = Timer(run)
+timer.start()
+for i in to_type:
+    msg = ''
+    while msg != i:
+        msg = input(f'QUICK!!! Type {i}: ')
+    timer.split()
+
+print(f'Congratulations! You took {timer.time()}s to type: {", ".join(to_type)}')
+timer.reset()
+print('\n'.join(f'{i.name} - {i.best}' for i in run.segments))
