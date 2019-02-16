63290ea83ca3765165a1b004e70ec390b2284cb9
matcool <26722564+matcool@users.noreply.github.com>
2019-02-16 19:42:49 -0300

    some changes

--- test.py
+++ test.py
@@ -7,13 +7,16 @@ for i in to_type:
     run.segments.append(Segment(name='Type '+i))
 
 timer = Timer(run)
-timer.start()
-for i in to_type:
-    msg = ''
-    while msg != i:
-        msg = input(f'QUICK!!! Type {i}: ')
-    timer.split()
+def do_run():
+    timer.start()
+    for i in to_type:
+        msg = ''
+        while msg != i:
+            msg = input(f'QUICK!!! Type {i}: ')
+        timer.split()
 
-print(f'Congratulations! You took {formatTime(timer.time())}s to type: {", ".join(to_type)}')
-timer.reset()
-print('\n'.join(f'{i.name} - {i.best}' for i in run.segments))
+    print(f'Congratulations! You took {formatTime(timer.time())}s to type: {", ".join(to_type)}')
+    timer.reset()
+    print('\n'.join(f'{i.name} - {i.best}' for i in run.segments))
+
+do_run()
