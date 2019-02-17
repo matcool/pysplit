04bf398e2ebf228bd9c9dfc6032c88d6261c23fd
matcool <26722564+matcool@users.noreply.github.com>
2019-02-16 22:59:59 -0300

    basic livesplit-like gui in pygame

---
+++ pysplit-gui.py
@@ -0,0 +1,69 @@
+import pygame, sys
+from pygame.locals import *
+import keyboard
+from pysplit import *
+
+# yes hardcoded file great code
+run = Run.from_json('test.json')
+timer = Timer(run)
+width, height = 300, 100
+segHeight = 50
+segGap = 10
+height += len(run.segments)*segHeight
+size = (width, height)
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+pygame.display.set_caption('PySplit')
+pygame.init()
+
+def start_or_split(*args):
+    if timer.state == TimerState.NOTHING:
+        timer.start()
+    elif timer.state == TimerState.RUNNING:
+        timer.split()
+    elif timer.state == TimerState.ENDED:
+        timer.reset()
+
+keyboard.on_press_key('f5',start_or_split)
+
+def draw_text(text,x,y,size,color=(255,255,255),font=None,align=None):
+    try:
+        font = pygame.font.SysFont(font, size)
+        surface = font.render(text,1,color)
+        xoff = 0
+        yoff = 0
+        if align == 'center':
+            xoff = -surface.get_width()/2
+            yoff = -surface.get_height()/2
+        elif align == 'right':
+            xoff = -surface.get_width()
+        screen.blit(surface,(x+xoff,y+yoff))
+    except pygame.error:
+        pass
+
+def draw_segments():
+    current_seg = len(timer.times)
+    for i, seg in enumerate(run.segments):
+        color = (255,255,255)
+        if timer.state != TimerState.NOTHING:
+            if i == current_seg:
+                color = (82,159,206)
+            elif i < current_seg:
+                color = (109,206,82)
+        draw_text(seg.name,0,segHeight*i+50,segHeight-segGap,color=color)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.display.quit()
+            sys.exit(0)
+        elif event.type == VIDEORESIZE:
+            size = width,height = event.size
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    screen.fill((0,0,0))
+
+    draw_segments()
+    draw_text(run.name,width/2,13,26,align='center')
+    draw_text(run.category,width/2,30,20,align='center')
+    draw_text(format_time(timer.time(force=True),decimal_places=2),width-10,height-50,40,font='ubuntumedium',align='right')
+
+    pygame.display.update() 
